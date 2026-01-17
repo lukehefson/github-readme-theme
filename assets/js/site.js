0c1861a70e580588e4a20d515a8b1e551136829a
Luke Hefson <lukehefson@gmail.com>
2026-01-17 17:00:56 -0400

    Tweaking

--- assets/js/site.js
+++ assets/js/site.js
@@ -335,7 +335,8 @@
 
   // Header anchor link functionality
   function createLinkIcon() {
-    return '🔗';
+    // Simple link icon (Feather icons style)
+    return '<svg viewBox="0 0 24 24" width="15" height="15" fill="none" stroke="#24292f" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>';
   }
 
   function initHeaderAnchorLinks() {
